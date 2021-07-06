a20c3cb4806d02d90d09e99dc0df6e5e3a9a8588
Madhu Nadamala <madhu.nadamala@outlook.com>
2021-07-05 22:35:35 -0700

    RunCalculateTax Complete: PDF Page 114

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -8,8 +8,10 @@ namespace WritingFunctions
         static void Main(string[] args)
         {
             RunsTimesTable();
+            WriteLine ("\nCaluclating Tax");
+            RunCalculateTax();
         }
-
+        #region TimesTable
         static void TimesTable(byte number)
         {
             Console.WriteLine($"This is {number} times table");
@@ -19,7 +21,9 @@ namespace WritingFunctions
             }   
             WriteLine();
         }
+        #endregion TimesTable
 
+        #region RunsTimesTable
         static void RunsTimesTable()
         {
             bool isNumber;
@@ -37,5 +41,71 @@ namespace WritingFunctions
             }
             while(isNumber);
         }
+        #endregion RunsTimesTable
+
+        #region RunCalculateTax
+
+       
+        static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+        {
+            decimal rate = 0.0M;
+            switch(twoLetterRegionCode)
+            {
+                case "CH": // Switzerland
+                    rate = 0.08M;
+                    break;
+                case "DK": // Denmark
+                case "NO": // Norway
+                    rate = 0.25M;
+                    break;
+                case "GB": // United Kingdon
+                case "FR": // France
+                    rate = 0.2M;
+                    break;
+                case "HU": // Hungary
+                    rate = 0.27M;
+                    break;
+                case "OR": // Oregon 
+                case "AK": // Alaska 
+                case "MT": // Montana
+                    rate = 0.0M;
+                    break;
+                case "ND": // North Dakota 
+                case "WI": // Wisconsin 
+                case "ME": // Maryland 
+                case "VA": // Virginia
+                    rate = 0.05M;
+                    break;
+                case "CA": // California
+                        rate = 0.0825M;
+                    break;
+                default: // most US states
+                    rate = 0.06M;
+                    break; 
+            }
+            return amount * rate;
+        }
+       
+        static void RunCalculateTax()
+        {
+            Write("Enter an amount: ");
+            string amountIntext = ReadLine();
+
+            Write("Enter a  two-letter region code: ");
+            string region = ReadLine();
+            
+            if (decimal.TryParse(amountIntext, out decimal amount))
+            {
+                decimal taxToPay = CalculateTax(amount, region);
+                WriteLine($"You must pay {taxToPay} in Sales Tax");
+            }
+            else
+            {
+                WriteLine("You did no enter valid amount!");
+            }
+            
+        }
+
+        #endregion RunCalculateTax
     }
 }
